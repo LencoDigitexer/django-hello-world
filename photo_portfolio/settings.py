0561c44e591416995455ce46ce433f9283b63c99
Илья Евсеенко <addminuser258@gmail.com>
2025-04-28 02:26:46 +0300

    ну допустим....

--- photo_portfolio/settings.py
+++ photo_portfolio/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 
 
 # ВОТ ЗДЕСЬ ПРОПИСЫВАЕМ ДОМЕН БУДУЩЕГО САЙТА
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', 'django-hello-world-one-ruby.vercel.app']
 
 
 # Application definition
